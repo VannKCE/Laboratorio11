942a53c1cf45c3b9de39c07b5ffa2de65ebc6476
vcondori <vcondorie@unsa.edu.pe>
2025-10-22 12:14:20 -0500

    Ejercicio 2

--- JS/ejercicio1.js
+++ JS/ejercicio1.js
@@ -1,20 +1,14 @@
 console.log("Ingrese sus notas: ");
 
-
 let suma = 0;
 for (let i =1; i<=5; i++){
    let nota = parseFloat(prompt("Nota " + i+": "));
-
-
    if(nota<=20 && nota >=0){
        suma+=nota;
    } else {
        console.log("Nota invàlida");
    }
+   let promedio = suma/5;
 }
-
-
-let promedio = suma/5;
-
 
 console.log("Su promedio es" + promedio);
